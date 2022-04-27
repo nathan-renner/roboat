9e09627547624d73b9a555741a2158386a5a29d6
ecastane <60262222+ecastane@users.noreply.github.com>
2022-04-27 19:52:23 -0400

    Updated Radio Controls

--- ino/boat.ino
+++ ino/boat.ino
@@ -9,9 +9,15 @@ Servo Servo2;
 Servo Servo3;
 
 bool IS_MANUAL_CONTROL = false;
-char remoteInstr[100];
+char remoteInstruc[100];
+String boatInstr = "";
 char gpsCoords[100];
 float lat, lon;
+
+int x, angle;
+String x_str, angle_str;
+String funcCall = "";
+int num = 0;
 
 void setup() {
     // Start the serial monitor on port 9600
@@ -90,53 +96,70 @@ void sendPacket() {
     memset(gpsCoords, 0, 100);
 }
 //
-//void readPacket() {
-//    Serial.readBytes(remoteInstr, 100);
-//    // decode and find spaces
-//
-//    if (remoteInstr[0] == 'F') {
-//        IS_MANUAL_CONTROL = false;
-//        return;
-//    }
-//
-//    int currSpace, prevSpace = 2;
-//    int x, angle;
-//    String x_str, angle_str;
-//    String funcCall = "";
-//    int paramNum = 0;
-//    
-//    for (int i = 2; i < 100; i++) {
-//        if (remoteInstr[i] == ' ') {
-//            currSpace = i;
-//
-//            switch (paramNum){
-//                case 0:
-//                    for (int j = prevSpace; j < currSpace; j++)
-//                        funcCall += remoteInstr[j];
-//                    prevSpace = currSpace;
-//                    paramNum++;
-//                    break;
-//
-//                case 1:
-//                    for (int j = prevSpace; j < currSpace; j++)
-//                        x_str += remoteInstr[j];
-//                    x = x_str.toInt();
-//                    prevSpace = currSpace;
-//                    paramNum++;
-//                    break;
-//
-//                case 2:
-//                    for (int j = prevSpace; j < currSpace; j++)
-//                        angle_str += remoteInstr[j];
-//                    angle = angle_str.toInt();
-//                    prevSpace = currSpace;
-//                    paramNum++;
-//                    break;
-//            }
-//        }
-//    }
-//    IS_MANUAL_CONTROL = true;
-//}
+void readPacket(char remoteInstr[]) {
+    //String boatInstr = "";
+    //Serial.readBytes(remoteInstr, 100);
+    for (int i =0; i<100; i++){
+      if(remoteInstr[i] == '/') {
+        break;
+      }
+      boatInstr += remoteInstr[i];
+    }
+    Serial.println(boatInstr);
+    // decode and find spaces
+
+    if (remoteInstr[0] == 'F' || remoteInstr[0] == 'T') {
+        Serial.println("Yes");
+        
+    } else {
+      return;
+    }
+    if (remoteInstr[0] == 'F') {
+      IS_MANUAL_CONTROL = false;
+      return;
+    }
+
+    int currSpace, prevSpace = 2;
+    //int x, angle;
+    //String x_str, angle_str;
+    //String funcCall = "";
+    int paramNum = 0;
+    
+    /*for (int i = 2; i < 100; i++) {
+        if (remoteInstr[i] == ' ') {
+            currSpace = i;
+
+            switch (paramNum){
+                case 0:
+                    for (int j = prevSpace; j < currSpace; j++)
+                        funcCall += remoteInstr[j];
+                    prevSpace = currSpace;
+                    Serial.println(funcCall);
+                    paramNum++;
+                    break;
+
+                case 1:
+                    for (int j = prevSpace; j < currSpace; j++)
+                        x_str += remoteInstr[j];
+                    x = x_str.toInt();
+                    prevSpace = currSpace;
+                    Serial.println(x);
+                    paramNum++;
+                    break;
+
+                case 2:
+                    for (int j = prevSpace; j < currSpace; j++)
+                        angle_str += remoteInstr[j];
+                    angle = angle_str.toInt();
+                    prevSpace = currSpace;
+                    Serial.println(angle);
+                    paramNum++;
+                    break;
+            }
+        }
+    }*/
+    IS_MANUAL_CONTROL = true;
+}
 
 void loop() {
 
@@ -149,7 +172,54 @@ void loop() {
     turnAndSetSpeedWithDelay(45, 50, 1000);
     turnAndSetSpeedWithDelay(0, 70, 1000);
     turnAndSetSpeedWithDelay(0, 0, 2000);
-//    readPacket();
+    Serial.readBytes(remoteInstruc, 100);
+    //Serial.println(remoteInstruc);
+    if(remoteInstruc[0] == 'F' || remoteInstruc[0] == 'T') {
+      for(int i = 2; i<100; i++) {
+        if(remoteInstruc[i]== '/') {
+          break;
+        }
+        boatInstr += remoteInstruc[i];
+      }
+    }
+    //Serial.println(boatInstr);
+
+    for (int i=0; i<boatInstr.length(); i++) {
+      if(boatInstr[i] == ';') {
+        num++;
+        i++;
+      }
+      //Serial.println(num);
+      if(num == 0) {
+        funcCall += boatInstr[i];
+      } else if(num == 1){
+        x_str += boatInstr[i];
+      } else if(num == 2) {
+        angle_str += boatInstr[i];
+      } else {
+        break;
+      }
+      
+    }
+    //Serial.println("=========");
+    //Serial.println(x_str);
+    //Serial.println(angle_str);
+    x = x_str.toInt();
+    angle = angle_str.toInt();
+    Serial.println("=====<>=====");
+    Serial.println("Function: " + funcCall);
+    Serial.print("X: ");
+    Serial.println(x);
+    Serial.print("Angle: ");
+    Serial.println(angle);
+    Serial.println("=====<>=====");
+    funcCall = "";
+    x_str = "";
+    angle_str = "";
+    boatInstr = "";
+    num =0;
+    //readPacket(remoteInstruc);
+    //Serial.println( x + " " + angle);
 //    if (IS_MANUAL_CONTROL) {
 //        turnAndSetSpeedWithDelay(x, angle, 50);
 //    } else {
@@ -159,4 +229,5 @@ void loop() {
 //            Serial.println(data);
 //        }
 //    }
+  delay(50);
 }
