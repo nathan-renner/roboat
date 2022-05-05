9980fe7c692bc7493e0700641d493f5b7ec65924
Nate Renner <nathanrenner00@gmail.com>
2022-05-05 14:36:25 -0400

    Added boat_test.ino

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -5,43 +5,47 @@
 #include <cmath>
 #include <fstream>
 
-// #include <wiringSerial.h> (installed on pi)
-//Let's add this to our rep
+#include <wiringPi.h>
 
 using namespace std;
 
-float location[2];
-float heading[2]; //Magnitude, angle
+// float location[2];
+// float heading[2]; //Magnitude, angle
 
-float marginOfError = 1;
+// float marginOfError = 1;
 
 // angle from -90 to 90
 // speed from 0 to 100
 // delay for current command
 void turnAndSetSpeedWithDelay(int angle, int speed, int delay = 50);
 
-void turn(string type="WIDE") { //WIDE, SHARP, VERY SHARP (make this a hashmap?)
-    float turningAngle = 0;
-    if (type == "VERY SHARP") {
-        turningAngle = 30;
-    } else if (type == "SHARP") {
-        turningAngle = 15;
-    } else {
-        turningAngle = 10;
-    }
-    //Add code to trigger turning function w/ Arduino
-}
+// void turn(string type="WIDE") { //WIDE, SHARP, VERY SHARP (make this a hashmap?)
+//     float turningAngle = 0;
+//     if (type == "VERY SHARP") {
+//         turningAngle = 30;
+//     } else if (type == "SHARP") {
+//         turningAngle = 15;
+//     } else {
+//         turningAngle = 10;
+//     }
+//     //Add code to trigger turning function w/ Arduino
+// }
 
 int main() {
-    ifstream reader;
-    reader.open("queue.txt");
-    reader.getline(heading, sizeof(heading));
+    wiringPiSetup();
+    pinMode(0, OUTPUT); // or use whatever pins we want
+    pinMode(1, INPUT);
+
+    // ifstream reader;
+    // reader.open("queue.txt");
+    // reader.getline(heading, sizeof(heading));
 
     while(true) {
+        String comm
+        delay(50);
         //Get location and heading from Arduino
-        cout << "Location: (" << location[0] << ", " << location[1] << ")" << endl;
-        cout << "Heading: " << heading[0] << " at " << heading[1] << " degrees" << endl;
-
+        // cout << "Location: (" << location[0] << ", " << location[1] << ")" << endl;
+        // cout << "Heading: " << heading[0] << " at " << heading[1] << " degrees" << endl;
     }
 }
 
