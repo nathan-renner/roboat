1c0b348c49ac89a0f2f61008573948ecb14d0f2c
Joey Rupert <joeyrupert731@gmail.com>
2022-05-13 12:32:18 -0400

    Removed legacy code from commModule.h

--- cpp/src/commModule.h
+++ cpp/src/commModule.h
@@ -1,7 +1,3 @@
-//Below is the code to communicate to Arduino via USB Serial
-
-//Use "g++ roboat.cpp -lwiringPi" to run. 
-//Don't forget "-lwiringPi" or it won't work.
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -77,17 +73,3 @@ public:
         return errcode;
     }
 };
-
-
-// int main () {
-//     int fd;
-
-//     if ((fd = serialOpen("/dev/ttyACM0, 9600")) < 0) {
-//         fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-//         return 1;
-//     }
-//     for (;;) {
-//         putchar(serialGetchar(fd));
-//         fflush(stdout);
-//     }
-// }
